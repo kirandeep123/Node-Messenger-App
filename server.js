c086eb340ab90b765e32acf80a2672cb9aa4e2c4
kirandeep123 <me@kiran.cc>
2019-07-05 02:01:16 +0530

    msgs incoming successfully

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ var express =require('express');
 var app =express()
 
 app.use(express.static(__dirname))
+
+
+var messages =[
+    {
+        name:'tim',
+        message :'buddy '
+    },
+    {
+        name:'jemmy',
+        messages:'cool'
+    }
+
+]
+app.get('/messages',(req,res)=>{
+    res.send(messages);
+})
 var server =app.listen(3000, ()=>{
     console.log("server is listening to port  " +server.address().port)
 });
