62cc596b652cbb786e9f11c8954b4abf7477cde6
kirandeep123 <me@kiran.cc>
2019-07-05 12:39:27 +0530

    socket functionality added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express =require('express');
 
 var bodyParser =require('body-parser')
 var app =express()
+var http =require('http').Server(app)
+var io =require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
@@ -29,6 +31,9 @@ app.get('/messages',(req,res)=>{
         messages.push(req.body);
         res.sendStatus(200);
     })
-var server =app.listen(3000, ()=>{
+    io.on('connection',(socket) =>{
+        console.log('user connected')
+    })
+var server =http.listen(3000, ()=>{
     console.log("server is listening to port  " +server.address().port)
 });
