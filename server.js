81d29ce8c2e36eeaf23d9b5a5fdccf0342ae7951
kirandeep123 <me@kiran.cc>
2019-07-05 12:30:59 +0530

    get and post methids

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 var express =require('express');
+
+var bodyParser =require('body-parser')
 var app =express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
 
+app.use(bodyParser.urlencoded({extended:false}))
 
 var messages =[
     {
-        name:'tim',
-        message :'buddy '
+        name:'Kiran',
+        message :'Hey Buddy '
     },
     {
-        name:'jemmy',
-        messages:'cool'
+        name:'Jemmy',
+        message:'I am Good ,Kiran'
     }
 
 ]
 app.get('/messages',(req,res)=>{
+ //   console.log(messages);
     res.send(messages);
 })
+
+ app.post('/messages',(req,res)=>{
+ //    console.log(req.body);
+        messages.push(req.body);
+        res.sendStatus(200);
+    })
 var server =app.listen(3000, ()=>{
     console.log("server is listening to port  " +server.address().port)
 });
